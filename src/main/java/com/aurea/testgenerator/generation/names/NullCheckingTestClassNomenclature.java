48fced4225fcb902eaca6b50241ed9e5084190fa
Igor  Velichkevich <i.velichkevich@besk.com>
2018-08-21 20:00:49 +0300

    start
    (cherry picked from commit 4051f4c)

---
+++ src/main/java/com/aurea/testgenerator/generation/names/NullCheckingTestClassNomenclature.java
@@ -0,0 +1,10 @@
+package com.aurea.testgenerator.generation.names;
+
+import com.aurea.testgenerator.source.Unit;
+
+public class NullCheckingTestClassNomenclature  implements TestClassNomenclature {
+    @Override
+    public String requestTestClassName(Unit unit) {
+        return unit.getClassName() + "PatternTest";
+    }
+}
