4051f4c1962e90b18b70067a260197d6f9274ad7
Igor  Velichkevich <i.velichkevich@besk.com>
2018-08-21 20:00:49 +0300

    start

---
+++ src/main/java/com/aurea/testgenerator/generation/names/NullCheckingTestClassNomenclatureFactory.java
@@ -0,0 +1,13 @@
+package com.aurea.testgenerator.generation.names;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("null-checking")
+public class NullCheckingTestClassNomenclatureFactory  implements TestClassNomenclatureFactory {
+    @Override
+    public TestClassNomenclature newTestClassNomenclature() {
+        return new StandardTestClassNomenclature();
+    }
+}
---
+++ src/main/java/com/aurea/testgenerator/generation/patterns/nullchecking/NullCheckingTestTypes.java
@@ -0,0 +1,7 @@
+package com.aurea.testgenerator.generation.patterns.nullchecking;
+
+import com.aurea.testgenerator.generation.TestType;
+
+public enum NullCheckingTestTypes implements TestType {
+    NULL_CHECING
+}
