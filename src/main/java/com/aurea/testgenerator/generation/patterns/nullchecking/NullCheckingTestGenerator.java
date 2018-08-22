ba92230a98caf3d979d9adaf73a37847207c8269
Ihar <igor.velichkevich@gmail.com>
2018-08-23 00:52:37 +0300

    fix for two args support

--- src/main/java/com/aurea/testgenerator/generation/patterns/nullchecking/NullCheckingTestGenerator.java
+++ src/main/java/com/aurea/testgenerator/generation/patterns/nullchecking/NullCheckingTestGenerator.java
@@ -21,6 +21,7 @@ import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.expr.Expression;
@@ -35,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,14 +47,17 @@ import org.springframework.stereotype.Component;
 @Profile("null-checking")
 public class NullCheckingTestGenerator implements TestGenerator {
     private static Logger logger = LogManager.getLogger(NullCheckingTestGenerator.class);
-    //Objects.requireNonNull(
-    //Stream.of(latitude, longitude).forEach(Objects::requireNonNull);
-    //requireNonNull(
+    // Objects.requireNonNull(
+    // requireNonNull(
+    // Stream.of(latitude, longitude).forEach(Objects::requireNonNull); //not supported
+    // final class as method argument not supported
+    // changed arg name (eg by =) is not supported
+    // invocations of requireNonNull for not args not implemented by requirements
+    // primitive types ignored
+    // method wrapper is not supported
 
-    private static final String PATTERTN_1 = "Objects.requireNonNull(";
-    private static final String PATTERTN_11 = "Objects.requireNonNull({1})";
-    private static final String PATTERTN_2 = "requireNonNull";
-    private static final String PATTERTN_4 = "java.util.Objects";
+    private static final String CALL_PATTERTN = "requireNonNull";
+    private static final String IMPORT_PATTERTN = "java.util.";
 
     @Autowired
     private TestGeneratorResultReporter reporter;
@@ -69,7 +74,7 @@ public class NullCheckingTestGenerator implements TestGenerator {
     @Override
     public Collection<TestGeneratorResult> generate(Unit unit) {
         if (unit.getCu().getImports().stream().map(ImportDeclaration::getName).noneMatch(n -> n.asString()
-                .startsWith(PATTERTN_4))) { //* is not supported
+                .startsWith(IMPORT_PATTERTN))) { // ToDo * is not supported
             return Collections.EMPTY_LIST;
         }
         List<ClassOrInterfaceDeclaration> classes = unit.getCu().findAll(ClassOrInterfaceDeclaration.class).stream()
@@ -78,7 +83,7 @@ public class NullCheckingTestGenerator implements TestGenerator {
         List<TestGeneratorResult> tests = new ArrayList<>();
 
         for (ClassOrInterfaceDeclaration classDeclaration : classes) {
-            if (!Callability.isInstantiable(classDeclaration) || !classDeclaration.toString().contains(PATTERTN_2)) {
+            if (!Callability.isInstantiable(classDeclaration) || !classDeclaration.toString().contains(CALL_PATTERTN) ) {
                 continue;
             }
 
@@ -86,18 +91,39 @@ public class NullCheckingTestGenerator implements TestGenerator {
             result.setType(NULL_CHECKING);
             for(MethodDeclaration methodDeclaration : classDeclaration.getMethods()) {
                 if (methodDeclaration.getParameters().isEmpty() || !methodDeclaration.getBody().isPresent()
-                        || !methodDeclaration.getBody().get().toString().contains(PATTERTN_2)) {
+                        //|| !methodDeclaration.getBody().get().toString().contains(CALL_PATTERTN)
+                        ) {
                     continue;
                 }
                 NodeList<Parameter> parameters = methodDeclaration.getParameters();
-                for (MethodCallExpr call : findMethodsCall(methodDeclaration, PATTERTN_2)) {
-                    parameters.forEach(p -> {
-                        if(call.getArguments().get(0).asNameExpr().getNameAsString().equals(p.getNameAsString())) {
-                            buildMethodTest(classDeclaration, methodDeclaration, p).ifPresent(o -> result.getTests().add(o));
+                for (MethodCallExpr call : findMethodsCall(methodDeclaration, CALL_PATTERTN)) {
+                    for (int i = 0; i < parameters.size(); i++) {
+                        Parameter parameter = parameters.get(i);
+                        if (!parameter.getType().isReferenceType()) {
+                            continue;
                         }
-                    });
+                        if (call.getArguments().get(0).asNameExpr().getNameAsString().equals(parameter.getNameAsString())) {
+                            buildMethodTest(classDeclaration, methodDeclaration, parameter, i)
+                                    .ifPresent(o -> result.getTests().add(o));
+                        }
+                    }
                 }
             }
+
+//            for(ConstructorDeclaration methodDeclaration : classDeclaration.getConstructors()) {
+//                if (methodDeclaration.getParameters().isEmpty() || !methodDeclaration.getBody().isPresent()
+//                        || !methodDeclaration.getBody().get().toString().contains(CALL_PATTERTN)) {
+//                    continue;
+//                }
+//                NodeList<Parameter> parameters = methodDeclaration.getParameters();
+//                for (MethodCallExpr call : findMethodsCall(methodDeclaration, CALL_PATTERTN)) {
+//                    parameters.forEach(p -> {
+//                        if(call.getArguments().get(0).asNameExpr().getNameAsString().equals(p.getNameAsString())) {
+//                            buildMethodTest(classDeclaration, methodDeclaration, p).ifPresent(o -> result.getTests().add(o));
+//                        }
+//                    });
+//                }
+//            }
 
             if (!result.getTests().isEmpty()) {
                 tests.add(result);
@@ -109,18 +135,20 @@ public class NullCheckingTestGenerator implements TestGenerator {
     private Optional<DependableNode<MethodDeclaration>> buildMethodTest(
             ClassOrInterfaceDeclaration classDeclaration,
             MethodDeclaration methodDeclaration,
-            Parameter parameter) {
+            Parameter parameter,
+            int order) {
         String fullTypeName = ASTNodeUtils.getFullTypeName(classDeclaration);
         InvocationBuilder invocationBuilder = new InvocationBuilder(valueFactory);
-        Map<String, DependableNode<Expression>> expressionsForParameters = new HashMap<>();
-        expressionsForParameters.put(parameter.getNameAsString(), DependableNode.from(new NullLiteralExpr()));
+        //Map<String, DependableNode<Expression>> expressionsForParameters = new HashMap<>();
+        //expressionsForParameters.put(parameter.getNameAsString(), DependableNode.from(new NullLiteralExpr()));
         Optional<DependableNode<ObjectCreationExpr>> maybeConstructor = invocationBuilder.build(classDeclaration.getConstructors().get(0));
-        Optional<DependableNode<MethodCallExpr>> maybeMethod = invocationBuilder.usingForParameters(expressionsForParameters).buildMethodInvocation(methodDeclaration);
+        Optional<DependableNode<MethodCallExpr>> maybeMethod = invocationBuilder.buildMethodInvocation(methodDeclaration);
         if (!maybeMethod.isPresent() || !maybeConstructor.isPresent()) {
             return Optional.empty();
         }
         DependableNode<MethodDeclaration> testMethod = new DependableNode<>();
         DependableNode<MethodCallExpr> method = maybeMethod.get();
+        method.getNode().getArguments().set(order, new NullLiteralExpr());
         TestNodeMerger.appendDependencies(testMethod, method);
         DependableNode<ObjectCreationExpr> constructor = maybeConstructor.get();
         TestNodeMerger.appendDependencies(testMethod, constructor);
@@ -144,8 +172,4 @@ public class NullCheckingTestGenerator implements TestGenerator {
     public static List<MethodCallExpr> findMethodsCall(Node node, String methodName) {
         return node.findAll(MethodCallExpr.class, n -> n.getNameAsString().equals(methodName));
     }
-
-//    public static List<MethodCallExpr> findMethodsCall(Node node, String methodName) {
-//        return node.findAll(MethodReferenceExpr.class, n -> n.getNameAsString().equals(methodName));
-//    }
 }
